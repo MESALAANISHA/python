f2843968740abaf0c33da5c09893676389f1147f
M.Anisha <92856983+MESALAANISHA@users.noreply.github.com>
2022-06-11 21:00:14 +0530

    Create hello world using functions.c

---
+++ hello world using functions.c
@@ -0,0 +1,20 @@
+//c program to print "hello world" on to the screen using functions.
+#include <stdio.h>
+
+ void print()
+ {
+     printf("HELLO WORLD");
+ }
+ int main()
+ {
+     print();
+ 
+     return 0;
+ }
+    
+
+
+
+-------OUTPUT--------
+  
+  HELLO WORLD
